3388125fcd978304ac87ccf830cdeabc1cf26b75
krishnakumars59809 <116875316+krishnakumars59809@users.noreply.github.com>
2022-11-14 11:05:54 -0500

    Mobile Response

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Header from './Component/header'
+import Landingpage from './Component/pages/Landingpage'
+import Sell from './Component/pages/Sellpage'
 
 const App = () => {
   return (
    <> 
-  <Routes>
-x   
-    <Route  path='/' element={<Header/>}/>
+  <Header/>
+  <Routes>   
+    <Route  path='/' element={<Landingpage/>}/>
+    <Route  path='/sell' element={<Sell/>}/>
 
   </Routes></>
    
--- src/Component/header/index.js
+++ src/Component/header/index.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../../App.css'
+import useMediaQuery from '../helper/hook/useMediaQuery'
+import Mobilenav from './Mobilenav'
+import {mobile} from '../helper/constatnt/ScreenSize'
+
+const NavItem=[{name:"Home",link:"/"},{name:"sell",link:"/sell"},{name:"Property Agents",link:"#"},{name:"About",link:"#"},{name:"Contactus",link:"#"}]
+
 const Header = () => {
-    const NavItem=[{name:"Home",link:"#"},{name:"Promotors",link:"#"},{name:"Property Agents",link:"#"},{name:"About",link:"#"},{name:"Contactus",link:"#"}]
-  return (
+   const [isMobileview]=useMediaQuery(mobile) ; 
+    return (
     <div className='flex items-center  justify-between p-5 bg-blue-300  '><h5>Real Estate</h5>
-
-     <div className='flex items-center gap-4 '>{NavItem.map((Nav,k)=>(
-    <NavLink key={k} to={Nav.link}>
-     {Nav.name}
-    </NavLink>
-
-     ))}</div>
+   { isMobileview?( <div className='flex items-center gap-4 '>{NavItem.map((Nav,k)=>(
+      <NavLink key={k} to={Nav.link ||"#" }>
+       {Nav.name}
+      </NavLink>
+  
+       ))}</div>):(<Mobilenav navItem={NavItem}/>)}
+    
     
     </div>
   )

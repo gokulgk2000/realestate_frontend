f63ba685995b9bb67cf8b7e13178f9be211ba63e
gokulgk2000 <110904612+gokulgk2000@users.noreply.github.com>
2022-11-25 17:24:37 +0530

    buyer modal update

--- src/Component/helper/backend_helpers.js
+++ src/Component/helper/backend_helpers.js
@@ -32,7 +32,16 @@ const userLogin = (payload) =>
   put(`${BASE_URL}/admin/removeProperty`, payload);
   const GETALLUSERSBYLIMIT = (payload) =>
   post(`${BASE_URL}/admin/getAllUsersPage`, payload);
+  const getuserdetails = (payload) =>
+  post(`${BASE_URL}/user/getUserById`, payload);
+  const getbuyerdetails = (payload) =>
+  post(`${BASE_URL}/buyer/getUserById`, payload);
+  const buyerReg = (payload) =>
+  post(`${BASE_URL}/buyer/buyerregister`, payload);
   export {userRegisteration,
+    getuserdetails,
+    buyerReg,
+    getbuyerdetails,
     PropertyRegistration,
     userLogin,
     getAllProperty,

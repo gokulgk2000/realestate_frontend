d8cb40cd132ab222d0eeb8b8db36db2d6311828f
krishnakumars59809 <116875316+krishnakumars59809@users.noreply.github.com>
2022-11-30 10:14:52 -0500

    profile update

--- src/Component/header/Navbar.js
+++ src/Component/header/Navbar.js
@@ -109,7 +109,7 @@ function Navbar() {
                             d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
                           />
                         </svg> */}
-                        <div className="pl-1 text-rose-700 font-serif hover:text-teal-700 hidden md:block lg:block">
+                        <div className="pl-2 text-rose-700 font-serif hover:text-teal-700 hidden md:block lg:block">
                             {user?.firstname} {user?.lastname}
                         </div>
                       </div>
--- src/Component/pages/auth/ProfileUpdate.js
+++ src/Component/pages/auth/ProfileUpdate.js
@@ -1,35 +1,74 @@
+
+
+import { Input } from '@material-tailwind/react';
 import React, { useEffect, useState } from 'react'
-import { updateProfileById } from '../../helper/backend_helpers';
+import { getUserById, updateProfileById } from '../../helper/backend_helpers';
+import FileInput from '../../reusable/FileInput';
 const ProfileUpdate = () => {
     const [user, setUser] = useState({});
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+
 
   const profileID = JSON.parse(localStorage.getItem("authUser"));
-
-console.log("view",user)
-const updateProfile = async () => {
-    const res = await updateProfileById({
-        userID: profileID?.userID
-        
+  console.log("res", user);
+  const getUserName = async () => {
+    const res = await getUserById({
+      userID: profileID?.userID,
     });
     if (res.success) {
-      setUser(res.user);
-      
+      setUser(res.User);
+      // console.log("res", res);
     }
   };
 
   useEffect(() => {
-    updateProfile();
+    getUserName();
   }, []);
+
+// console.log("view",user)
+// const updateProfile = async () => {
+//     const res = await updateProfileById({
+//         userID: profileID?.userID
+        
+//     });
+//       setUser(res.user);
+      
+//     }
+//   };
+
+//   useEffect(() => {
+//     updateProfile();
+//   }, []);
   return (
     <div>
 <div className='grid'>
-    {/* <div>
+    <div className=''>
        
-FirstName:{user?.firstname}
+FirstName: <Input
+              type="text"
+              name="firstname"
+              placeholder="Enter the firstname "
+              value={user?.firstname}
+         
+              onChange={(e) => setFirstname(e.target.value)}
+            />
 <div>
-lastName:{user?.lastname}</div>
-ProfilePicture:
-    </div>  */}
+lastName:<Input
+              type="text"
+              name="lastname"
+              placeholder="Enter the lastname "
+              value={user?.lastname}
+         
+              onChange={(e) => setLastname(e.target.value)}
+            /></div><div>
+ProfilePicture:<FileInput 
+              label="Property Images"
+              multiple={true}
+              accept=".png, .jpg, .jpeg,.pdf,.webp"
+             /></div>
+    </div> 
+    
 </div>
 <button className='bg-blue-500'  onClick={(e)=>{}}>Edit</button>
 

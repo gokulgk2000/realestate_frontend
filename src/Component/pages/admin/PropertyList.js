4ed9ba67b7946ea18a655540879d107d481d3af2
krishnakumars59809 <116875316+krishnakumars59809@users.noreply.github.com>
2022-12-27 13:49:46 -0500

    UI update

--- src/Component/pages/admin/PropertyList.js
+++ src/Component/pages/admin/PropertyList.js
@@ -181,13 +181,13 @@ await getAllProperties()
                     <ul className=" text-sm font-medium text-gray-900 rounded-lg  dark:text-white">
     <li className="w-full ">
         <div className="flex items-center pl-3">
-            <input
+  {PropertyData?.status === "approved" &&        <input
              type="checkbox" 
             //  name="facilities"
             checked={PropertyData?.isPremium}
              className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" 
               onChange={(e)=>updatePremiumProperties(e.target.checked,PropertyData?._id)}
-          />
+          />}  
         
             <label for="vue-checkbox" className="py-3 ml-2 w-full text-sm font-medium dark:text-gray-300" >premium</label>
         </div>

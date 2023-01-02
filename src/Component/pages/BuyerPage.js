165b51a4c895097681977ff825eebb10a59c8e6d
padmanaban_d <dspadmanaban2000@gmail.com>
2023-01-02 19:27:06 +0530

    search on buyer page

--- src/Component/pages/BuyerPage.js
+++ src/Component/pages/BuyerPage.js
@@ -1,7 +1,9 @@
+import { Breadcrumbs } from '@material-tailwind/react';
 import { map } from 'lodash';
 import React from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { getinterestbyId } from '../helper/backend_helpers';
 import { useQuery } from '../helper/hook/useQuery';
 import { useUser } from './contextProvider/UserProvider';
@@ -10,7 +12,7 @@ import PropertyCard from './PropertyCard';
 const BuyerPage = () => {
   const query = useQuery();
   const { currentUser, setCurrentUser } = useUser();
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(true);
   const [interest,setInterest] = useState([])
   const [Interest, setInterestSuccess] = useState();
   const [interesterror,setInterestError] = useState([]);
@@ -39,6 +41,28 @@ console.log("interest",interest)
   } 
   handleFetchInterested()
 },[])
+if(interest?.length===0){
+  return(
+    <div className="py-10">
+       <Breadcrumbs>
+          <Link to="/">
+            <button className="opacity-60 font">Home</button>
+          </Link>
+        </Breadcrumbs>
+       
+  <p className=" flex justify-center py-10 font-semibold">
+    
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
+</svg>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
+</svg>
+
+    No interested Properties Found...</p>
+  </div>
+  )
+}
   return (
     <div> {isLoading ? (
       <div className="flex justify-center py-5  "> Loading...</div>
@@ -69,8 +93,8 @@ console.log("interest",interest)
         {interest
          ?.filter(
           (item) =>
-            item?.propertyId?.location.includes(searchText.toString().toLowerCase()) ||
-            item?.propertyId?.title.includes(searchText.toString().toLowerCase()) 
+            item?.propertyId?.location?.toString().toLowerCase().includes(searchText.toString().toLowerCase()) ||
+            item?.propertyId?.title?.toString().toLowerCase().includes(searchText.toString().toLowerCase()) 
               )
               // .slice((currentPage - 1) * 10, currentPage * 10)
         .map((pro, i) => (

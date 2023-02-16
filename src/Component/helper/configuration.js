efd5970a728ae69ecd1c128c10c02066b5341270
gokulgk2000 <110904612+gokulgk2000@users.noreply.github.com>
2023-02-17 00:33:03 +0530

    Update configuration.js

--- src/Component/helper/configuration.js
+++ src/Component/helper/configuration.js
@@ -2,10 +2,6 @@
 // export const SERVER_URL = "https://covai-realestate-4a993.web.app"
 // export const SERVER_URL = "https://realestate-backend.vercel.app"
 // export const SERVER_URL = "https://realestate-backend.vercel.app"
-export const SERVER_URL = "https://realestatebackend.azurewebsites.net/"
-
+export const SERVER_URL = "https://realestatebackend.azurewebsites.net/";
 
 // export const SERVER_URL = "https://realestatecbebackend.azurewebsites.net"
-
-
-

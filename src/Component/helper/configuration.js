14630ddb362e6d5c0132297502e8a952862bbc72
gokulgk2000 <110904612+gokulgk2000@users.noreply.github.com>
2023-02-26 20:57:22 +0530

    update

--- src/Component/helper/configuration.js
+++ src/Component/helper/configuration.js
@@ -1,5 +1,6 @@
 // export const SERVER_URL = "http://localhost:8000"
 // export const SERVER_URL = "https://covai-realestate-4a993.web.app"
+
 export const SERVER_URL = "https://realestate-backend.vercel.app"
 
 
